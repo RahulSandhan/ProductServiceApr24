c8b6a6c2df0ae2bcce07c832d6ab3cb05c76f7ca
RahulSandhan <rahulv.sandhanshive@gmail.com>
2024-06-08 19:53:51 +0530

    Implementing OAuth-2

--- src/main/java/com/example/apr24firstapi/controllers/ProductController.java
+++ src/main/java/com/example/apr24firstapi/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package com.example.apr24firstapi.controllers;
 
+import com.example.apr24firstapi.components.AuthUtils;
 import com.example.apr24firstapi.dtos.CreateProductRequestDto;
 import com.example.apr24firstapi.models.Product;
 import com.example.apr24firstapi.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -16,9 +19,12 @@ import java.util.List;
 public class ProductController {
     private ProductService productService;
 
+    private AuthUtils authUtils;
+
     @Autowired
-    public ProductController(@Qualifier("selfProduct") ProductService productService) {
+    public ProductController(@Qualifier("selfProduct") ProductService productService, AuthUtils authUtils) {
         this.productService = productService;
+        this.authUtils = authUtils;
     }
 
     //GET localhost:8080/products/1
@@ -31,11 +37,16 @@ public class ProductController {
         return new ArrayList<Product>();
     }
 
-    @PostMapping("")
-    public Product createProduct(@RequestBody CreateProductRequestDto requestDto){
+    @PostMapping("/")
+    public ResponseEntity<Product> createProduct(@RequestBody CreateProductRequestDto requestDto, @RequestHeader("Auth") String token){
         //validate the data
+        if(!authUtils.validateToken(token)){  //if token is not valid, then we are going to return null
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED); // if token is not valid, we will give them Unauthorized(401) or Forbidden(403)
+        }
 
-        return productService.createProduct(requestDto.getTitle(), requestDto.getDescription(),
+
+        Product product =  productService.createProduct(requestDto.getTitle(), requestDto.getDescription(),
                 requestDto.getImage(), requestDto.getPrice(), requestDto.getCategoryName());
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 }
